31361bca3e9db9287702214744aa06028d20d279
Mashtaler Olha <mashtaler887@gmail.com>
2021-12-18 23:05:37 +0200

    fix error video, add const

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,21 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 function saveTime(e) {
   let currentTime = e.seconds;
 
-  localStorage.setItem('videoplayer-current-time', currentTime);
+  localStorage.setItem(CURRENT_TIME_KEY, currentTime);
 }
 
-const trotlledSaveTime = throttle((e) => saveTime(e), 1000);
+const trotlledSaveTime = throttle(e => saveTime(e), 1000);
 
 player.on('timeupdate', trotlledSaveTime);
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+if (localStorage.getItem(CURRENT_TIME_KEY)) {
+  player.setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY));
+}
